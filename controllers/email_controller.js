5f77164ca292c99e0aed67c7f098d0f10139c391
Luis Plata <www.luisplata@gmail.com>
2024-02-27 17:23:16 -0500

    validate if the email is valid

--- controllers/email_controller.js
+++ controllers/email_controller.js
@@ -5,11 +5,15 @@ class UserController {
 
   saveEmail(req, res) {
     const { email } = req.body;
+    //validate if email have a valid format and is not empty
+    if (!email || !email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/)) {
+      res.status(400).json({ error: 'Invalid email', message: 'Invalid email' });
+      return;
+    }
     EmailModel.saveEmail(email).then((result) => {
       logger.info([result, 'savedEmail in UserController.saveEmail']);
       res.json({ email: result.email });
-    }
-    ).catch((error) => {
+    }).catch((error) => {
       logger.error(error);
       res.status(500).json({ error: error.message, message: error.message });
     });
